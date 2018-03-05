12290576bb905f4ae1810a611325a4585a619aa6
dedefer <ddf1998@gmail.com>
2018-03-05 22:19:36 +0300

    deleted binary, tree done

--- tree/tree.go
+++ tree/tree.go
@@ -9,59 +9,94 @@ import (
 	"path/filepath"
 )
 
-func init() {
-
+// функция фильтрации директорий
+func onlyDirs(files []os.FileInfo) []os.FileInfo {
+	result := make([]os.FileInfo, 0, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			result = append(result, file)
+		}
+	}
+	return result
 }
 
-func recursiveTree(out io.Writer, path string, printFiles bool, offset string) error {
+func recursiveTree(out io.Writer, path string, printFiles bool, offset string, lastOffset string) error {
+	// Получаем полный путь
 	path, err := filepath.Abs(path)
-	fmt.Printf("%s%s\n", offset, path[len(filepath.Dir(path))+1:])
-	if len(offset) == 0 {
-		offset = "├── "
-	} else {
-		offset = "│   " + offset
-	}
 	if err != nil {
 		return err
 	}
 
+	// выводим название текущей папки с учетом текущего отступа
+	fmt.Println(offset + lastOffset + path[len(filepath.Dir(path))+len(string(os.PathSeparator)):])
+
+	// наращиваем отступ для подкаталогов в зависимости от последнего отступа
+	// (последний элемент в списке или нет)
+	if lastOffset == "├───" {
+		offset += "│   "
+	} else if lastOffset == "└───" {
+		offset += "    "
+	}
+
+	// получаем все каталоги в директории
 	fileSlice, err := ioutil.ReadDir(path)
 	if err != nil {
 		return err
 	}
 
-	for _, file := range fileSlice {
-		if file.IsDir() {
-			recursiveTree(out, path+string(os.PathSeparator)+file.Name(), printFiles, offset)
+	// фильтруем их если не установлен флаг показывать файлы
+	if !printFiles {
+		fileSlice = onlyDirs(fileSlice)
+	}
+
+	numberOfLastFile := len(fileSlice) - 1
+
+	// запускаем цикл по файлам
+	for i, file := range fileSlice {
+
+		// устанавливаем следующий последний отступ в зависимости от того
+		// последний файл в директории или нет
+		if i == numberOfLastFile {
+			lastOffset = "└───"
 		} else {
-			if printFiles {
-				out.Write([]byte(fmt.Sprintf("%s%s (%dB)\n", offset, file.Name(), file.Size())))
-			}
+			lastOffset = "├───"
 		}
+
+		// если файл является директорией то
+		// рекурсивно вызываем функцию со следующим отступом
+		// если нет то печатаем имя файла
+		if file.IsDir() {
+			recursiveTree(out, path+string(os.PathSeparator)+file.Name(), printFiles, offset, lastOffset)
+		} else if printFiles {
+			out.Write([]byte(fmt.Sprintf("%s (%dB)\n", offset+lastOffset+file.Name(), file.Size())))
+		}
+
 	}
 	return nil
 
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
+	// находим полный путь до заданной директории
+	// и если нет ошибок то вызываем recursiveTree
 	_, err := ioutil.ReadDir(path)
-	if err == nil {
-		return recursiveTree(out, path, printFiles, "")
+	if err != nil {
+		return err
 	}
-	return err
+	return recursiveTree(out, path, printFiles, "", "")
 }
 
 func main() {
-
-	var targetPath string
-	var printFiles bool
+	// создаем переменные для флагов и парсим флаги
+	targetPath := ""
+	printFiles := false
 	flag.StringVar(&targetPath, "p", ".", "target path")
 	flag.BoolVar(&printFiles, "f", false, "print files")
 	flag.Parse()
 
+	// выводим ошибку если она была
 	err := dirTree(os.Stdout, targetPath, printFiles)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
